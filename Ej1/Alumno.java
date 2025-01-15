af10d32f46cb00e18a7094685211ffd1dce74b5c
BraisPN <braispose15@gmail.com>
2025-01-15 09:18:38 +0100

    Ejercicio java clases

---
+++ Ej1/Alumno.java
@@ -0,0 +1,32 @@
+package Ej1;
+public class Alumno extends Persona{
+    private String  codigoXade;
+    private boolean autorizaTutores;
+
+    public Alumno(){
+
+    }
+
+    public String getCodigoXade() {
+        return codigoXade;
+    }
+
+    public void setCodigoXade(String codigoXade) {
+        this.codigoXade = codigoXade;
+    }
+
+    public boolean isAutorizaTutores() {
+        return autorizaTutores;
+    }
+
+    public void setAutorizaTutores(boolean autorizaTutores) {
+        this.autorizaTutores = autorizaTutores;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno [codigoXade=" + codigoXade + ", autorizaTutores=" + autorizaTutores + "]";
+    }
+
+    
+}
