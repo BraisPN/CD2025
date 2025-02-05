fb3d3e8e0295c203ed3cbbea7672a9658b51f1f7
braispn <braispose15@gmail.com>
2025-02-05 09:32:57 +0100

    Copia repo

---
+++ Ej1/probas/ud03/probas/src/test/java/com/braispose/TestMatematica.java
@@ -0,0 +1,20 @@
+package com.braispose;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMatematica {
+    
+    @Test
+    public void fib(){
+        Matematica mat = new Matematica();
+        assertEquals(mat.fib(0), 0);
+        assertEquals(mat.fib(1), 1);
+        assertEquals(mat.fib(2), 1);
+        assertEquals(mat.fib(3), 2);
+        assertEquals(mat.fib(7), 13);
+        assertEquals(mat.fib(8), 21);
+    }
+}
