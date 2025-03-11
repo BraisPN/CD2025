f9ea754e5b7e308a06bc929342e5e74cb5c411ae
braispn <braispose15@gmail.com>
2025-03-11 13:21:26 +0100

    Validar Contraseña Terminado

--- TDD/Proxecto3/demo/src/main/java/com/braispose/ValidadorContraseña.java
+++ TDD/Proxecto3/demo/src/main/java/com/braispose/ValidadorContraseña.java
@@ -1,23 +1,23 @@
 package com.braispose;
 public class ValidadorContraseña {
 
-    public boolean ValidadorContraseña(String contrasinal){
+    public boolean esValida(String contrasinal){
         if (contrasinal.length() < 8) return false;
         int cont = 0;
-        for(int i = 0; i < 8;i++){
+        for(int i = 0; i < contrasinal.length(); i++){ 
             if ((contrasinal.charAt(i) >= 'A' && contrasinal.charAt(i) <= 'Z')) cont++;
         }
         if (cont == 0) return false;
 
         int cont2 = 0;
-        for(int i = 0; i < 8;i++){
+        for(int i = 0; i < contrasinal.length(); i++){ 
             if ((contrasinal.charAt(i) >= 'a' && contrasinal.charAt(i) <= 'z')) cont2++;
         }
         if (cont2 == 0) return false;
 
         int cont3 = 0;
-        for(int i = 0; i < 8;i++){
-            if ((contrasinal.charAt(i) >= '0' && contrasinal.charAt(i) <= '9')) cont2++;
+        for(int i = 0; i < contrasinal.length(); i++){ 
+            if ((contrasinal.charAt(i) >= '0' && contrasinal.charAt(i) <= '9')) cont3++;
         }
         if (cont3 == 0) return false;
         if (!(contrasinal.matches(".*[!@#$%^&*].*")))return false;
